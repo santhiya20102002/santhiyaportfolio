2b9be645c40c310d5aa0b538c67af5e64e5d8616
santhiya20102002 <santhiya.t2020ece@sece.ac.in>
2025-07-26 11:51:44 +0530

    Your message for the second commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,35 +1,34 @@
 import React from 'react';
-import profile from './profile.png'; // Replace with your actual image path
-import './Hero.css'
+import profile from './profile.png';
+import './Hero.css';
+
 const Hero = () => {
   return (
     <header id="hero">
-      <div className="container hero-container text-center" style={{display:"flex"}}>
-        
-        <div style={{paddingLeft:"250px"}}>
-        <h1 className="hero-title animate__animated animate__fadeInUp animate__delay-1s">
-          Hi, I'm <span className="highlight">Santhiya</span>
-        </h1>
-         <h3 className="hero-subtitle animate__animated animate__fadeInUp animate__delay-2s" style={{fontSize:"30px",color:"burlywood"}}>
-          Fullstack Developer
-        </h3>
-        <p className="hero-subtitle animate__animated animate__fadeInUp animate__delay-2s">
+      <div className="hero-container">
+        <div className="hero-text">
+          <h1 className="hero-title animate__animated animate__fadeInUp animate__delay-1s">
+            Hi, I'm <span className="highlight">Santhiya</span>
+          </h1>
+          <h3 className="hero-subtitle animate__animated animate__fadeInUp animate__delay-2s">
+            Fullstack Developer
+          </h3>
+          <p className="hero-skills animate__animated animate__fadeInUp animate__delay-2s">
             ReactJS | Bootstrap | JavaScript | Expressjs | Nodejs | Mongodb
-        </p>
-        <a
-          href="#projects"
-          className="btn btn-outline-light hero-btn animate__animated animate__fadeInUp animate__delay-3s"
-        >
-          View My Work
-        </a>
+          </p>
+          <a
+            href="#projects"
+            className="btn btn-outline-light hero-btn animate__animated animate__fadeInUp animate__delay-3s"
+          >
+            View My Work
+          </a>
         </div>
-        <div style={{paddingLeft:"250px"}}>
-        <img
-          src={profile}
-          alt="Santhiya"
-          className="hero-img animate__animated animate__fadeInDown"
-          style={{height:"300px",width:"250px"}} 
-        />
+        <div className="hero-image">
+          <img
+            src={profile}
+            alt="Santhiya"
+            className="hero-img animate__animated animate__fadeInDown"
+          />
         </div>
       </div>
     </header>
